1afa4818e031c96cd2fef6e969c903adfe946819
List <>
2024-02-26 18:37:11 +0800

    no message

---
+++ Example/WPlayerKit/WPViewController.m
@@ -0,0 +1,29 @@
+//
+//  WPViewController.m
+//  WPlayerKit
+//
+//  Created by lishutian@wjacloud.com on 02/26/2024.
+//  Copyright (c) 2024 lishutian@wjacloud.com. All rights reserved.
+//
+
+#import "WPViewController.h"
+
+@interface WPViewController ()
+
+@end
+
+@implementation WPViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Example/WPlayerKit/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  WPlayerKit
+//
+//  Created by lishutian@wjacloud.com on 02/26/2024.
+//  Copyright (c) 2024 lishutian@wjacloud.com. All rights reserved.
+//
+
+@import UIKit;
+#import "WPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WPAppDelegate class]));
+    }
+}
