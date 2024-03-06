9d0e04b20f71b3065591e9a88821d3dd0b29c804
List <>
2024-03-06 15:06:58 +0800

    no message

--- Example/Pods/Target Support Files/WPlayerKit/WPlayerKit-umbrella.h
+++ Example/Pods/Target Support Files/WPlayerKit/WPlayerKit-umbrella.h
@@ -10,6 +10,28 @@
 #endif
 #endif
 
+#import "MediaCallBackDelegate.h"
+#import "MediaClient.h"
+#import "MediaHeader.h"
+#import "MessageManager.h"
+#import "NetApiManager.h"
+#import "P2PPlayManager.h"
+#import "P2PRecordVideoManager.h"
+#import "PlayerManager.h"
+#import "Server.h"
+#import "WCommonLink.h"
+#import "WCommonMacro.h"
+#import "WCommonStream.h"
+#import "WMediaControl.h"
+#import "WPlayerKit.h"
+#import "WVideoCaptureView.h"
+#import "WVideoPlayView.h"
+#import "WVideoRecorderView.h"
+#import "WZoomSuperView.h"
+#import "ZDKBeautyControl.h"
+#import "ZDKMediaCallBack.h"
+#import "ZDKPlayVideoView.h"
+#import "ZDKSpeakerVoice.h"
 
 FOUNDATION_EXPORT double WPlayerKitVersionNumber;
 FOUNDATION_EXPORT const unsigned char WPlayerKitVersionString[];
